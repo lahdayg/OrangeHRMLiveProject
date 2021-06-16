edebaa1d3b0a071768498e4fd24d434516e2ff22
lahdayg <46688273+lahdayg@users.noreply.github.com>
2021-06-16 19:59:47 +0100

    Update BaseTest.cs

--- OrangeHRM/Hooks/BaseTest.cs
+++ OrangeHRM/Hooks/BaseTest.cs
@@ -18,11 +18,17 @@ namespace OrangeHRM.Hooks
         [BeforeScenario]
         public void BeforeScenario()
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--incognito");
+            CChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--headless");
+            //options.AddArgument("--incognito");
+            
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            Console.WriteLine("Setup");
 
             driver = new ChromeDriver(options);
-            driver.Manage().Window.Maximize();
+            //driver.Manage().Window.Maximize();
         }
 
         [AfterScenario]
